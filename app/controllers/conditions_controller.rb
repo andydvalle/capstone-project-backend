38f93341cd37b52dedd82d779534cbe1396cd16b
github username <github email address>
2020-07-15 20:21:33 -0700

    create index controller actions

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -1,2 +1,6 @@
 class ConditionsController < ApplicationController
+    def index
+        conditions = Condition.all
+        render json: conditions.to_json
+    end
 end
--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -1,2 +1,6 @@
 class MedicationsController < ApplicationController
+    def index
+        medications = Medication.all
+        render json: medications.to_json
+    end
 end
