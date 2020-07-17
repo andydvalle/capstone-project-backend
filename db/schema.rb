b96f9ac848447cd25be08cdce3d58b1897359975
github username <github email address>
2020-07-16 17:51:34 -0700

    remove description from conditions

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_16_170103) do
+ActiveRecord::Schema.define(version: 2020_07_17_004812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "conditions", force: :cascade do |t|
     t.string "name"
-    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
