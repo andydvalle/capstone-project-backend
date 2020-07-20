99e88bf23b430e0cfebbb59510db1f216afa093e
github username <github email address>
2020-07-19 18:01:09 -0700

    add create to patients controllers

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::PatientsController < ApplicationController
         patients = Patient.all
         render json: patients.to_json(include: [:conditions, :medications])
     end
+
+    def new
+        patient = Patient.new
+    end
+
+    def create
+        patient = Patient.create(patient_params)
+        render json: patient.to_json(include: [:conditions, :medications])
+    end
+
+    private
+    
+    def patient_params
+        params.require(:patient).permit(:firstName, :lastName, :dob, :allergies, :user_id)
+    end
+
 end
