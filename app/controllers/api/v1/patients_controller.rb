834dd5e61906339839710b2d36f7024478eb7e29
github username <github email address>
2020-07-20 15:29:08 -0700

    include appointments and clinics to patients index

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::PatientsController < ApplicationController
     def index
         patients = Patient.all
         #filter here grab only patient from current_user (from headers auth jwt token)
-        render json: patients.to_json(include: [:conditions, :medications])
+        render json: patients.to_json(include: [:conditions, :medications, :appointments, :clinics])
     end
 
     def new
