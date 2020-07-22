e154709ad7e7be9a6835d24fa1e26047220fae4e
github username <github email address>
2020-07-22 09:54:52 -0700

    add destroy action to patients

--- app/controllers/api/v1/patients_controller.rb
+++ app/controllers/api/v1/patients_controller.rb
@@ -16,6 +16,11 @@ class Api::V1::PatientsController < ApplicationController
         patient = Patient.create(patient_params)
         render json: patient.to_json(include: [:conditions, :medications, :appointments, :clinics])
     end
+    
+    def destroy
+        patient = Patient.find(params[:id])
+        patient.destroy
+    end
 
     private
     
