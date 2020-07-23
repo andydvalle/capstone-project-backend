24392de87f63062deb997a2eff2083fcf2173e97
github username <github email address>
2020-07-23 12:41:51 -0700

    add edit update destroy actions to aappointment

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -13,6 +13,22 @@ class Api::V1::AppointmentsController < ApplicationController
         render json: appointment.to_json(include: [:patient, :clinic])
     end
 
+        def edit
+        appointment = Appointment.find(params[:id])
+    end
+
+    def update
+        appointment = Appointment.find(params[:id])
+        appointment.update(appointment_params)
+        render json: appointment.to_json
+    end
+    
+    def destroy
+        appointment = Appointment.find(params[:id])
+        appointment.destroy
+        render json: appointment.to_json
+    end
+
     private
     
     def appointment_params
