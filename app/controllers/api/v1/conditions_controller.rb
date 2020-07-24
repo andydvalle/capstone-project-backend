965927106384868c34e2c03831d572a0dd0e5bef
github username <github email address>
2020-07-23 17:58:36 -0700

    add edit destory actions to conditions

--- app/controllers/api/v1/conditions_controller.rb
+++ app/controllers/api/v1/conditions_controller.rb
@@ -13,6 +13,22 @@ class Api::V1::ConditionsController < ApplicationController
         render json: condition.to_json(include: [:patient])
     end
 
+    def edit
+        condition = Condition.find(params[:id])
+    end
+
+    def update
+        condition = Condition.find(params[:id])
+        condition.update(condition_params)
+        render json: condition.to_json
+    end
+    
+    def destroy
+        condition = Condition.find(params[:id])
+        condition.destroy
+        render json: condition.to_json
+    end
+
     private
     
     def condition_params
