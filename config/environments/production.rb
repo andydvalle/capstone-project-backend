bd13c417c7be8ec888ef5e1039b4b2fc2775a53c
github username <github email address>
2020-08-13 15:47:20 -0700

    comment out config.assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.assets.compress = true
+  # config.assets.compress = true
  
 end
